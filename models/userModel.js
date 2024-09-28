83981393c0d3809beb4c4e35a8f33bead4794c2a
MdTamimJD <tamimoli75285@gmail.com>
2024-09-28 23:11:35 +0600

    database server done

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,12 @@
-const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/deploy")
-
+const mongoose = require("mongoose");
+;( async ()=>{
+    try{
+        await mongoose.connect("mongodb+srv://tamim:tamim123@cluster0.fe9fn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    } catch (error) {
+        console.log("Error: ",error)
+        throw error
+    }
+})()
 const userModel = mongoose.Schema({
     fullname:String,
     dob:String,
